da78557529a2c750503483ea8dbd5170874e9a65
martletko <44828878+martletko@users.noreply.github.com>
2020-07-06 16:46:16 +1000

    Add files via upload

---
+++ interFace2.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'interFace2.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(876, 735)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+
+        self.Enter_Login = QtGui.QLineEdit(self.centralwidget)
+        self.Enter_Login.setGeometry(QtCore.QRect(50, 60, 191, 22))
+        self.Enter_Login.setObjectName(_fromUtf8("Enter_Login"))
+
+        self.Date_And_Time = QtGui.QDateTimeEdit(self.centralwidget)
+        self.Date_And_Time.setGeometry(QtCore.QRect(50, 30, 194, 22))
+        self.Date_And_Time.setObjectName(_fromUtf8("Date_And_Time"))
+
+        self.tableWidget = QtGui.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(20, 100, 821, 571))
+        self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+
+        '''
+        self.MyProgressBar = QtGui.QProgressBar(self.centralwidget)
+        self.MyProgressBar.setGeometry(QtCore.QRect(710, 50, 118, 23))
+        self.MyProgressBar.setProperty("value", 24)
+        self.MyProgressBar.setObjectName(_fromUtf8("MyProgressBar"))
+        '''
+
+        self.Sort_Date = QtGui.QPushButton(self.centralwidget)
+        self.Sort_Date.setGeometry(QtCore.QRect(270, 20, 93, 28))
+        self.Sort_Date.setObjectName(_fromUtf8("Sort_Date"))
+
+        self.Sort_Login = QtGui.QPushButton(self.centralwidget)
+        self.Sort_Login.setGeometry(QtCore.QRect(270, 60, 93, 28))
+        self.Sort_Login.setObjectName(_fromUtf8("Sort_Login"))
+
+        self.Save_Button = QtGui.QPushButton(self.centralwidget)
+        self.Save_Button.setGeometry(QtCore.QRect(380, 60, 93, 28))
+        self.Save_Button.setObjectName(_fromUtf8("Save_Button"))
+
+        self.Clear_Button = QtGui.QPushButton(self.centralwidget)
+        self.Clear_Button.setGeometry(QtCore.QRect(490, 60, 93, 28))
+        self.Clear_Button.setObjectName(_fromUtf8("Clear_Button"))
+
+        self.Print_Button = QtGui.QPushButton(self.centralwidget)
+        self.Print_Button.setGeometry(QtCore.QRect(600, 60, 93, 28))
+        self.Print_Button.setObjectName(_fromUtf8("Print_Button"))
+
+        self.PageLineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.PageLineEdit.setGeometry(QtCore.QRect(710, 60, 40, 22))
+        self.PageLineEdit.setObjectName(_fromUtf8("enter_page_Num"))
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 876, 26))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.Sort_Date.setText(_translate("MainWindow", "сорт Дата", None))
+        self.Sort_Login.setText(_translate("MainWindow", "сорт Логин", None))
+        self.Save_Button.setText(_translate("MainWindow", "сохранить", None))
+        self.Clear_Button.setText(_translate("MainWindow", "очистить", None))
+        self.Print_Button.setText(_translate("MainWindow", "вывести", None))
+
---
+++ new3.py
@@ -0,0 +1,161 @@
+import sys
+import time
+import os
+import csv
+import sip
+sip.setapi('QString', 2)
+sip.setapi('QVariant', 2)
+
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import SIGNAL
+from PyQt4.QtGui import QApplication, QMainWindow, QFont, QLineEdit, QFormLayout
+
+from interFace2 import Ui_MainWindow
+
+import datetime
+
+import multiprocessing as mp
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None, *args, **kwargs):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+        logFolderName, ok = QtGui.QInputDialog.getText(self, "ввод логина", "введите логин для поиска", QtGui.QLineEdit.Normal)
+        self.logPath = os.path.join(os.getcwd(), logFolderName)
+
+        self.Sort_Date.clicked.connect(self.on_Sort_Date_clicked)
+
+        self.Sort_Login.clicked.connect(self.on_Sort_Login_clicked)
+
+        self.Save_Button.clicked.connect(self.on_Save_Button_clicked)
+
+        self.Clear_Button.clicked.connect(self.on_Clear_Button_clicked)
+
+        self.Print_Button.clicked.connect(self.call_async)
+
+        self.myPool = mp.Pool(processes=1)
+
+    def call_async(self):
+        num_of_page = self.PageLineEdit.text()
+        logPath1 = self.logPath
+        self.myPool.apply_async(func=on_Print_Button_clicked_read, args=(num_of_page, logPath1), callback=self.on_Print_Button_clicked)
+
+    def on_Sort_Date_clicked(self):
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(0)
+        logs = os.listdir(self.logPath)
+
+        data = self.Date_And_Time.text()
+        data = data.split(" ")
+        date1 = data[0]
+        minutes = data[1]
+        date1 = date1.split(".")
+        minutes = minutes.split(":")
+        unix_time = int(datetime.datetime(int(date1[2]), int(date1[1]),int(date1[0]), int(minutes[0]), int(minutes[1]), int(minutes[2])).timestamp())
+        unix_time += 36000
+        unix_time = str(unix_time)
+        print_data = []
+        for log in logs:
+            log_path = os.path.join(self.logPath, log)
+            with open(log_path, "r") as fileInput:
+                for row in csv.reader(fileInput):
+                    if str(unix_time) in row: 
+                        print_data.append(row)
+        if print_data == []:
+            pass
+        else:
+            self.tableWidget.setRowCount(len(print_data))
+            self.tableWidget.setColumnCount(len(print_data[0]))
+            for i in range(0, len(print_data)):
+                tokens = print_data[i]
+                for j in range(len(tokens)):
+                    obj = QtGui.QTableWidgetItem(tokens[j])
+                    self.tableWidget.setItem(i,j,obj)
+            self.tableWidget.resizeColumnsToContents()
+
+    def on_Sort_Login_clicked(self):
+        text = self.Enter_Login.text()
+        logs = os.listdir(self.logPath)
+        print_data = []
+        for log in logs:
+            log_path = os.path.join(self.logPath, log)
+            with open(log_path, "r") as fileInput:
+                for row in csv.reader(fileInput):
+                    if text in row:
+                        print_data.append(row)
+        if print_data == []:
+            pass
+        else:
+            self.tableWidget.setRowCount(len(print_data))
+            self.tableWidget.setColumnCount(len(print_data[0]))
+            for i in range(0, len(print_data)):
+                tokens = print_data[i]
+                for j in range(len(tokens)):
+                    obj = QtGui.QTableWidgetItem(tokens[j])
+                    self.tableWidget.setItem(i,j,obj)
+            self.tableWidget.resizeColumnsToContents()
+
+    def on_Save_Button_clicked(self):
+        number_of_rows = self.tableWidget.rowCount()
+        number_of_columns = self.tableWidget.columnCount()
+        fileName, ok = QtGui.QInputDialog.getText(self, "ввод ", "введите имя файла без расширения", QtGui.QLineEdit.Normal)
+        fileName += '.csv'
+        file_path = os.path.join(self.logPath, fileName)
+        if number_of_rows == 0:
+            pass
+        else:
+            for i in range(0,number_of_rows):
+                row = []
+                for j in range(0,number_of_columns):
+                    tmp_item = self.tableWidget.item(i,j).text()
+                    row.append(tmp_item)
+                with open(fileName, 'a') as myFile:
+                    myWriter = csv.writer(myFile, delimiter=',')
+                    myWriter.writerow(row)
+
+    def on_Print_Button_clicked(self, hundred):
+        for j in range(len(hundred)):
+            self.tableWidget.setRowCount(len(hundred))
+            self.tableWidget.setColumnCount(len(hundred[0]))
+            tokens = hundred[j]
+            for k in range(0, len(tokens)):
+                obj = QtGui.QTableWidgetItem(tokens[k])
+                self.tableWidget.setItem(j,k,obj)
+
+
+    def on_Clear_Button_clicked(self):
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(0)
+
+def on_Print_Button_clicked_read(num_of_page, logPath1):
+    try:
+        num = int(num_of_page)
+        logs = os.listdir(logPath1)
+        print_data = []
+        for log in logs:
+            log_path = os.path.join(logPath1, log)
+            with open(log_path, "r") as fileInput:
+                for row in csv.reader(fileInput):
+                    print_data.append(row)
+        
+        hundred = []
+        for i in range((num-1)*100, num*100, 1):
+            try:
+                hundred.append(print_data[i])
+            except IndexError:
+                pass
+    except TypeError:
+        pass
+    return hundred
+
+
+def main():
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()    
